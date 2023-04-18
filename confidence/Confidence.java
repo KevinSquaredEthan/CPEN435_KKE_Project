2bf4f3fb697bf55a1e56286effc2bf893ba2170e
Ethan Higa <eleehiga808@gmail.com>
2023-04-18 20:13:00 +0000

    adding case sensitive and skip stop words code

--- confidence/Confidence.java
+++ confidence/Confidence.java
@@ -2,6 +2,13 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -12,6 +19,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
 
 public class Confidence {
 
@@ -20,11 +30,50 @@ public class Confidence {
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
+    // for caseSensitive
+    private boolean caseSensitive;
+    private Set<String> patternsToSkip = new HashSet<String>();
+    private Configuration conf;
+    private BufferedReader fis;
+
+    @Override
+    public void setup(Context context) throws IOException,
+		InterruptedException {
+	conf = context.getConfiguration();
+	caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
+	if (conf.getBoolean("wordcount.skip.patterns", false)) {
+		URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
+		for (URI patternsURI : patternsURIs) {
+			Path patternsPath = new Path(patternsURI.getPath());
+			String patternsFileName = patternsPath.getName().toString();
+			parseSkipFile(patternsFileName);
+		}
+	}
+    }
+
+    private void parseSkipFile(String fileName) {
+	try {
+		fis = new BufferedReader(new FileReader(fileName));
+		String pattern = null;
+		while ((pattern = fis.readLine()) != null) {
+			patternsToSkip.add(pattern);
+		}
+	} catch (IOException ioe) {
+		System.err.println("Caught exception while parsing the cached file '"
+		+ StringUtils.stringifyException(ioe));
+	}
+     }
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
       ArrayList<String> A = new ArrayList<String>(); // String not Text, as sort works with strings
-      StringTokenizer itr = new StringTokenizer(value.toString(),"\n"); // iterate through each line
+      // caseSensitive and remove stop words
+      String line = (caseSensitive) ?
+	value.toString() : value.toString().toLowerCase();
+      for (String pattern : patternsToSkip) {
+	line = line.replaceAll(pattern, "");
+      }
+      StringTokenizer itr = new StringTokenizer(line,"\n"); // iterate through each line
       while (itr.hasMoreTokens()) { 
 	StringTokenizer itr2 = new StringTokenizer(itr.nextToken(), " ");
 	// this loop will find a list of unique words in a line
@@ -66,13 +115,29 @@ public class Confidence {
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "term pair count");
+    GenericOptionsParser optionParser = new GenericOptionsParser(conf,
+	args);
+    String[] remainingArgs = optionParser.getRemainingArgs();
+    if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
+	System.err.println("Usage: confidence <in> <out> [-skip skipPatternFile]");
+	System.exit(2);
+    }
+    Job job = Job.getInstance(conf, "confidence");
     job.setJarByClass(Confidence.class);
     job.setMapperClass(TokenizerMapper.class);
     job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
+    List<String> otherArgs = new ArrayList<String>();
+    for (int i=0; i < remainingArgs.length; ++i) {
+	if ("-skip".equals(remainingArgs[i])) {
+		job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+		job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+	} else {
+		otherArgs.add(remainingArgs[i]);
+	}
+    }
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
--- confidence/start.sh
+++ confidence/start.sh
@@ -1,4 +1,4 @@
 hadoop fs -mkdir /user/ehiga
 hadoop fs -mkdir /user/ehiga/confidence
 hadoop fs -put ./input /user/ehiga/confidence
-hadoop jar confidence.jar Confidence /user/ehiga/confidence/input /user/ehiga/confidence/output
+hadoop jar confidence.jar Confidence -Dwordcount.case.sensitive=false /user/ehiga/confidence/input /user/ehiga/confidence/output -skip /user/ehiga/confidence/patterns.txt
